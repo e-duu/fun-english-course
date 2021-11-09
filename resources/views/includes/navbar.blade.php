1ef6f1aaff5b40c7640c1aa0b21b0733451bb017
e-duu <ramangart2@gmail.com>
2021-11-09 20:12:33 +0700

    first commit

---
+++ resources/views/includes/navbar.blade.php
@@ -0,0 +1,16 @@
+<header class="bg-[pink]">
+  <div class="container-fluid px-20">
+    <div class="flex justify-between items-center py-5">
+      <img src="{{ asset('/images/logo.png') }}" class="w-80" alt="fun english course logo">
+      <img src="{{ asset('/images/edge-logo.png') }}" class="w-24" alt="edge logo">
+    </div>
+  </div>
+  <nav class="flex justify-end bg-yellow-500 py-5">
+    <ul class="flex space-x-20 px-20">
+      <li class="font-bold text-white text-xl"><a href="">HOME</a></li>
+      <li class="font-bold text-white text-xl"><a href="">LEARNING RESOURCES</a></li>
+      <li class="font-bold text-white text-xl"><a href="">PAYMENT</a></li>
+    </ul>
+  </nav>
+  <div class="border-b-8 border-blue-700"></div>
+</header>
