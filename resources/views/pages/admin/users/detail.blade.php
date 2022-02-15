2093fd2ebf6714f2b3c7902a33d65fc6255377c2
e-duu <ramangart2@gmail.com>
2022-02-15 10:23:49 +0700

    done

--- resources/views/pages/admin/users/detail.blade.php
+++ resources/views/pages/admin/users/detail.blade.php
@@ -10,14 +10,16 @@
     <button formaction="{{ route('user.all') }}" class="px-5 py-1 bg-yellow-400 rounded-md font-semibold text-white sm:absolute sm:right-0 w-full sm:w-auto">Back to User</button>
   </form>
   <div class="bg-white shadow-md p-6 text-left rounded-lg sm:flex space-x-8">
-    <img src="{{ asset('/users/' . $data->photo) }}" alt="user profile photo" class="w-60 sm:w-96 sm:max-h-96 rounded-md">
-    <div class="flex-col space-y-3">
-      <p class="text-sm sm:text-lg"><b> Name :</b> &nbsp;&nbsp; {{ $data->name }}</p>
-      <p class="text-sm sm:text-lg"><b> Username :</b> &nbsp;&nbsp; {{ $data->username }}</p>
-      <p class="text-sm sm:text-lg"><b> Role :</b> &nbsp;&nbsp; {{ $data->role }}</p>
-      <p class="text-sm sm:text-lg"><b> Email :</b> &nbsp;&nbsp; {{ $data->email }}</p>
-      <p class="text-sm sm:text-lg"><b> Enroll in Levels :</b></p>
-
+    <img src="{{ asset('/users/' . $data->photo) }}" alt="user profile photo" class="w-60 sm:w-96 rounded-md">
+    <div class="flex-col">
+      <div class="flex-col space-y-3">
+        <p class="text-sm sm:text-lg"><b> Name :</b> &nbsp;&nbsp; {{ $data->name }}</p>
+        <p class="text-sm sm:text-lg"><b> Username :</b> &nbsp;&nbsp; {{ $data->username }}</p>
+        <p class="text-sm sm:text-lg"><b> Role :</b> &nbsp;&nbsp; {{ $data->role }}</p>
+        <p class="text-sm sm:text-lg"><b> Email :</b> &nbsp;&nbsp; {{ $data->email }}</p>
+        <p class="text-sm sm:text-lg"><b> Enroll in Levels :</b></p>
+      </div>
+      
       <div class="w-full overflow-hidden rounded-lg shadow-xs">
         <div class="w-full overflow-x-auto">
           <table class="w-full whitespace-no-wrap">
