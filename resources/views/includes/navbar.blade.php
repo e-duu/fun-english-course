d78e52a244d0f8379ab0d27024b9cab34301b3bd
e-duu <ramangart2@gmail.com>
2022-04-11 15:50:09 +0700

    commit

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -15,7 +15,7 @@
       <li class="absolute right-4 top-1 sm:top-5 sm:right-20">
         <div x-data="{ dropdownOpen: false }" class="relative">
           <button @click="dropdownOpen = !dropdownOpen" class="relative z-10 ml-8 sm:ml-0">
-            <img src="{{ asset('/users/' . Auth::user()->photo) }}" alt="user profile photo" class="w-8 sm:w-12 rounded-full">
+            <img src="{{ asset('/users/' . Auth::user()->photo) }}" alt="user profile photo" class="h-8 w-8 sm:h-12 sm:w-12 rounded-full">
           </button>
       
           <div x-show="dropdownOpen" @click="dropdownOpen = false" class="fixed inset-0 h-full w-full z-10"></div>
