a7fc7b00c158bc6604bf7e8cc6270bc049d72a9b
Hudzaifah <hudz1357@gmail.com>
2022-04-26 06:47:47 +0700

    kepo commit

--- app/Exports/StudentExport.php
+++ app/Exports/StudentExport.php
@@ -5,16 +5,34 @@ namespace App\Exports;
 use App\Models\Student;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Maatwebsite\Excel\Concerns\WithEvents;
 use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithMapping;
+use Maatwebsite\Excel\Concerns\WithStyles;
+use Maatwebsite\Excel\Events\AfterSheet;
+use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
 
-class StudentExport implements FromCollection, WithHeadings, ShouldAutoSize
+class StudentExport implements FromCollection, WithHeadings, ShouldAutoSize, WithMapping, WithStyles, WithEvents
 {
-    /**
-    * @return \Illuminate\Support\Collection
-    */
     public function collection()
     {
-        return Student::all();
+        return Student::with(['user', 'sppMonth'])->get();
+    }
+
+    public function map($student) : array
+    {
+        return [
+            $student->id,
+            $student->user->detail->parent,
+            $student->user->detail->city,
+            $student->user->detail->country,
+            $student->user->detail->status,
+            $student->sppMonth->level->program->name,
+            $student->sppMonth->level->name,
+            $student->sppMonth->price > 10000 ? 'USD' : 'IDR',
+            $student->sppMonth->price,
+            $student->sppMonth->status,
+        ];
     }
 
     public function headings() : array
@@ -30,6 +48,26 @@ class StudentExport implements FromCollection, WithHeadings, ShouldAutoSize
             'Currency',
             'Price',
             'Status Payment',
+        ];
+    }
+
+    public function styles(Worksheet $sheet)
+    {
+        return [
+            1 => ['font' => ['bold' => true]],
+        ];
+    }
+
+    public function registerEvents(): array
+    {
+        return [
+            AfterSheet::class => function(AfterSheet $event) {
+
+                $event->sheet->getDelegate()->getStyle(1)
+                    ->getAlignment()
+
+                    ->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER);
+            },
         ];
     }
 }
