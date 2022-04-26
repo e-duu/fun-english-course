38b4f5555616193d52a53921c1f1bdefd5ffebd2
Hudzaifah <hudz1357@gmail.com>
2022-04-27 06:11:55 +0700

    kepo commit

--- resources/views/pages/admin/students/detail-student.blade.php
+++ resources/views/pages/admin/students/detail-student.blade.php
@@ -8,72 +8,120 @@ Detail Students - {{ $data->name }}
 @section('content')
 
 <div class="flex item-center justify-between space-x-2">
-	{{-- Modal Upload --}}
-	<div x-data="{ showModal : false }">
-		<!-- Button -->
-		<button @click="showModal = !showModal" class="px-4 py-2 text-sm bg-blue-600 rounded-md transition-colors duration-150 ease-linear text-white focus:outline-none focus:ring-0 font-semibold hover:bg-blue-700">Send Mail</button>
+    <div class="flex space-x-2 items-center">
+        {{-- Modal Upload --}}
+        <div x-data="{ showModal : false }">
+            <!-- Button -->
+            <button @click="showModal = !showModal" class="px-4 py-2 text-sm bg-blue-600 rounded-md transition-colors duration-150 ease-linear text-white focus:outline-none focus:ring-0 font-semibold hover:bg-blue-700">Send Mail</button>
 
-		<!-- Modal Background -->
-		<div x-show="showModal" class="fixed text-gray-500 flex items-center justify-center overflow-auto z-50 bg-black bg-opacity-40 left-0 right-0 top-0 bottom-0" x-transition:enter="transition ease duration-300" x-transition:enter-start="opacity-0" x-transition:enter-end="opacity-100" x-transition:leave="transition ease duration-300" x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0">
-				<!-- Modal -->
-				<div x-show="showModal" class="bg-white rounded-xl shadow-2xl p-6 w-80 sm:w-3/6 mx-10" @click.away="showModal = false" x-transition:enter="transition ease duration-100 transform" x-transition:enter-start="opacity-0 scale-90 translate-y-1" x-transition:enter-end="opacity-100 scale-100 translate-y-0" x-transition:leave="transition ease duration-100 transform" x-transition:leave-start="opacity-100 scale-100 translate-y-0" x-transition:leave-end="opacity-0 scale-90 translate-y-1">
+            <!-- Modal Background -->
+            <div x-show="showModal" class="fixed text-gray-500 flex items-center justify-center overflow-auto z-50 bg-black bg-opacity-40 left-0 right-0 top-0 bottom-0" x-transition:enter="transition ease duration-300" x-transition:enter-start="opacity-0" x-transition:enter-end="opacity-100" x-transition:leave="transition ease duration-300" x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0">
+                    <!-- Modal -->
+                    <div x-show="showModal" class="bg-white rounded-xl shadow-2xl p-6 w-80 sm:w-3/6 mx-10" @click.away="showModal = false" x-transition:enter="transition ease duration-100 transform" x-transition:enter-start="opacity-0 scale-90 translate-y-1" x-transition:enter-end="opacity-100 scale-100 translate-y-0" x-transition:leave="transition ease duration-100 transform" x-transition:leave-start="opacity-100 scale-100 translate-y-0" x-transition:leave-end="opacity-0 scale-90 translate-y-1">
+                        <!-- Title -->
+                        <span class="font-bold block text-2xl mb-3">Send To Mail </span>
+                        <div class="border-b border-gray-500 mb-5"></div>
+                        <!-- Send To Email (Invoice) -->
+                        <form action="" method="POST" enctype="multipart/form-data">
+                            @csrf
+                            @method('POST')
+                            <div class='flex items-center justify-center w-full'>
+                                <table class="w-full whitespace-no-wrap">
+                                    <thead>
+                                        <tr class="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b dark:border-gray-700 bg-gray-50 dark:text-gray-400 dark:bg-gray-800">
+                                            <th class="px-4 py-3">Student Name</th>
+                                            <th class="px-4 py-3">Price</th>
+                                            <th class="px-4 py-3"><input id="checkAll" type="checkbox"></th>
+                                        </tr>
+                                    </thead>
+                                    <tbody class="bg-white divide-y dark:divide-gray-700 dark:bg-darker">
+
+                                        @forelse ($spps as $item)
+                                            <tr class="text-gray-700 dark:text-gray-400">
+                                                <td class="px-4 py-3 text-sm">
+                                                    {{ $item->student->name }}
+                                                </td>
+                                                <td class="px-4 py-3 text-sm">
+                                                    {{'Rp '.number_format($item->price) }}
+                                                </td>
+                                                <td class="px-4 py-3 text-sm">
+                                                    <input type="checkbox" name="level">
+                                                </td>
+                                            </tr>
+                                        @empty
+                                            <tr>
+                                                <td colspan="3" class="text-center text-gray-500 px-4 py-3">
+                                                    <p>
+                                                        Data is empty..
+                                                    </p>
+                                                </td>
+                                            </tr>
+                                        @endforelse
+                                    </tbody>
+                                </table>
+                            </div>
+                            <div class="border-b border-gray-500 my-5"></div>
+
+                            <!-- Buttons -->
+                            <div class="flex-col sm:flex-row sm:justify-end text-center sm:text-right space-y-2 sm:space-y-0 sm:space-x-2 mt-5">
+                            <button type="button" @click="showModal = !showModal" class="w-full sm:w-auto sm:px-4 py-2 text-xs sm:text-sm bg-red-600 rounded-md transition-colors duration-150 ease-linear text-white focus:outline-none focus:ring-0 font-semibold hover:bg-red-700">Cancel</button>
+
+                            <button type="submit" class="w-full sm:w-auto sm:px-4 py-2 text-xs sm:text-sm bg-blue-600 rounded-md transition-colors duration-150 ease-linear text-white focus:outline-none focus:ring-0 font-semibold hover:bg-blue-700">Submit</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+        {{-- Modal Filter --}}
+        <div x-data="{ showModal : false }">
+            <!-- Button -->
+            <button @click="showModal = !showModal" class="px-4 py-2 text-sm bg-blue-600 rounded-md transition-colors duration-150 ease-linear text-white focus:outline-none focus:ring-0 font-semibold hover:bg-blue-700">Create Spp</button>
+
+            <!-- Modal Background -->
+            <div x-show="showModal" class="fixed text-gray-500 flex items-center justify-center overflow-auto z-50 bg-black bg-opacity-40 left-0 right-0 top-0 bottom-0" x-transition:enter="transition ease duration-300" x-transition:enter-start="opacity-0" x-transition:enter-end="opacity-100" x-transition:leave="transition ease duration-300" x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0">
+                    <!-- Modal -->
+                <div x-show="showModal" class="bg-white rounded-xl shadow-2xl p-6 w-80 sm:w-3/6 mx-10" @click.away="showModal = false" x-transition:enter="transition ease duration-100 transform" x-transition:enter-start="opacity-0 scale-90 translate-y-1" x-transition:enter-end="opacity-100 scale-100 translate-y-0" x-transition:leave="transition ease duration-100 transform" x-transition:leave-start="opacity-100 scale-100 translate-y-0" x-transition:leave-end="opacity-0 scale-90 translate-y-1">
                     <!-- Title -->
-                    <span class="font-bold block text-2xl mb-3">Send To Mail </span>
+                    <span class="font-bold block text-2xl mb-3">Create Spp </span>
                     <div class="border-b border-gray-500 mb-5"></div>
-                    <!-- Send To Email (Invoice) -->
-                    <form action="" method="POST" enctype="multipart/form-data">
-                        @csrf
-                        @method('POST')
-                        <div class='flex items-center justify-center w-full'>
-                            <table class="w-full whitespace-no-wrap">
-                                <thead>
-                                    <tr class="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b dark:border-gray-700 bg-gray-50 dark:text-gray-400 dark:bg-gray-800">
-                                        <th class="px-4 py-3">Student Name</th>
-                                        <th class="px-4 py-3">Price</th>
-                                        <th class="px-4 py-3"><input id="checkAll" type="checkbox"></th>
-                                    </tr>
-                                </thead>
-                                <tbody class="bg-white divide-y dark:divide-gray-700 dark:bg-darker">
+                    <!-- Some beer 🍺 -->
+                    <form action="{{route('student.show', $data->id)}}" method="GET">
+                        <label class="block mt-4 text-sm">
+                            <span class="text-gray-700 dark:text-gray-400">
+                                Student
+                            </span>
+                            <select name="user_id" class="block w-full mt-1 text-sm dark:text-gray-300 dark:border rounded-md border-gray-400 -gray-600 dark:bg-gray-700 form-select focus:border-blue-400 focus:outline-none focus:shadow-outline-blue dark:focus:shadow-outline-gray">
+                                <option selected>Choose Option...</option>
+                                {{-- @foreach ($users as $user)
+                                    <option value="{{ $user->id }}" >{{ $user->name }}</option>
+                                @endforeach --}}
+                            </select>
+                        </label>
 
-                                    @forelse ($spps as $item)
-                                        <tr class="text-gray-700 dark:text-gray-400">
-                                            <td class="px-4 py-3 text-sm">
-                                                {{ $item->student->name }}
-                                            </td>
-                                            <td class="px-4 py-3 text-sm">
-                                                {{'Rp '.number_format($item->price) }}
-                                            </td>
-                                            <td class="px-4 py-3 text-sm">
-                                                <input type="checkbox" name="level">
-                                            </td>
-                                        </tr>
-                                    @empty
-                                        <tr>
-                                            <td colspan="3" class="text-center text-gray-500 px-4 py-3">
-                                                <p>
-                                                    Data is empty..
-                                                </p>
-                                            </td>
-                                        </tr>
-                                    @endforelse
-                                </tbody>
-                            </table>
-                        </div>
+                        <label class="block mt-2 text-sm">
+                            <span class="text-gray-700 dark:text-gray-400">
+                                Price
+                            </span>
+                            <input name="price" type="text" class="block w-full mt-1 text-sm dark:text-gray-300 dark:border rounded-md border-gray-400 -gray-600 dark:bg-gray-700 form-select focus:border-blue-400 focus:outline-none focus:shadow-outline-blue dark:focus:shadow-outline-gray">
+                        </label>
+
                         <div class="border-b border-gray-500 my-5"></div>
 
                         <!-- Buttons -->
-                        <div class="flex-col sm:flex-row sm:justify-end text-center sm:text-right space-y-2 sm:space-y-0 sm:space-x-2 mt-5">
-                        <button type="button" @click="showModal = !showModal" class="w-full sm:w-auto sm:px-4 py-2 text-xs sm:text-sm bg-red-600 rounded-md transition-colors duration-150 ease-linear text-white focus:outline-none focus:ring-0 font-semibold hover:bg-red-700">Cancel</button>
+                        <div class="flex-col sm:flex-row sm:justify-end text-center sm:text-right space-y-2 sm:space-x-2 mt-5">
+                            <button type="button" @click="showModal = !showModal" class="w-full sm:w-auto sm:px-4 py-2 text-xs sm:text-sm bg-gray-600 rounded-md transition-colors duration-150 ease-linear text-white focus:outline-none focus:ring-0 font-semibold hover:bg-gray-700">Cancel</button>
 
-                        <button type="submit" class="w-full sm:w-auto sm:px-4 py-2 text-xs sm:text-sm bg-blue-600 rounded-md transition-colors duration-150 ease-linear text-white focus:outline-none focus:ring-0 font-semibold hover:bg-blue-700">Submit</button>
-                    </form>
+                            <button formaction="{{route('student.show', $data->id)}}" class="w-full sm:w-auto sm:px-4 py-2 text-xs sm:text-sm bg-blue-600 rounded-md transition-colors duration-150 ease-linear text-white focus:outline-none focus:ring-0 font-semibold hover:bg-blue-700">Submit </button>
+
+                        </form>
+                    </div>
                 </div>
             </div>
-		</div>
-	</div>
-  <div class="float-right mb-5">
-    <a href="{{ route('student.show', $data->program_id) }}" class="px-5 py-1 bg-yellow-400 rounded-md font-semibold text-white">Back to Level</a>
-  </div>
+        </div>
+    </div>
+    <div class="float-right mb-5">
+        <a href="{{ route('student.show', $data->program_id) }}" class="px-5 py-1 bg-yellow-400 rounded-md font-semibold text-white">Back to Level</a>
+    </div>
 </div>
 
 
