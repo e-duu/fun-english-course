27a165ac04398b4879ab0adeb7278085771a5b14
muji101 <mujikuwat91@gmail.com>
2022-05-25 21:03:12 +0700

    fix

--- resources/views/pages/payment.blade.php
+++ resources/views/pages/payment.blade.php
@@ -117,11 +117,11 @@
           </tbody>
         </table>
         </div>
-        {{-- <div class="grid px-4 py-3 text-xs font-semibold tracking-wide text-gray-500 uppercase border-t dark:border-gray-700 bg-gray-50 sm:grid-cols-9 dark:text-gray-400 dark:bg-gray-800">
+        <div class="grid px-4 py-3 text-xs font-semibold tracking-wide text-gray-500 uppercase border-t dark:border-gray-700 bg-gray-50 sm:grid-cols-9 dark:text-gray-400 dark:bg-gray-800">
           <div class="text-center w-auto sm:w-[565px] md:w-[1160px] 2xl:w-[1495px] ">
-            {{ $data->links() }}
+            {{-- {{ $data->links() }} --}}
           </div>
-        </div> --}}
+        </div>
       </div>
 
       <div class="w-full overflow-hidden rounded-lg shadow-xs mt-5">
