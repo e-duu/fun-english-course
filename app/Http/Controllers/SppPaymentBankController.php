8a29d0031585f1d58922fe332903ed27a9d6a1ad
muji101 <mujikuwat91@gmail.com>
2022-06-04 21:17:12 +0700

    fix moota

--- app/Http/Controllers/SppPaymentBankController.php
+++ app/Http/Controllers/SppPaymentBankController.php
@@ -115,10 +115,19 @@ class SppPaymentBankController extends Controller
 
                 // send email to company
                 Mail::send('pages.emails.SuccessMail', ['data' => $student], function ($message) use ($dataEmail, $pdf) {
-                    $message->to('edge.edukasi@gmail.com', 'Payment Success To Fun English Course')
+                    $message
+                        ->to('edge.edukasi@gmail.com', 'Payment Success To Fun English Course')
                         ->subject('Payment Success To Fun English Course')
                         ->attachData($pdf->output(), $this->namePdf);
+
+                    $message
+                        ->from($dataEmail["email"], 'Success Payment To Fun English Course');
                 });
+
+                // Mail::send('pages.emails.ReceiptMail', ['data' => $student], function ($message) use ($dataEmail) {
+                //     $message->to('edge.edukasi@gmail.com', 'Success Payment To Fun English Course')->subject('Success Payment Spp');
+                //     $message->from($dataEmail["email"], 'Success Payment To Fun English Course');
+                // });
 
                 // $store = SppPaymentBank::create([
                 //     'spp_month_id' => $spp->id,
