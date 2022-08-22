05af2f1b05165dbda6256ffe635c68a86510f7e5
muji101 <mujikuwat91@gmail.com>
2022-08-22 13:01:46 +0700

    update import invoice

--- app/Imports/StudentImport.php
+++ app/Imports/StudentImport.php
@@ -20,49 +20,64 @@ class StudentImport implements ToModel, WithStartRow
     }
 
     /**
-    * @param array $row
-    *
-    * @return \Illuminate\Database\Eloquent\Model|null
-    */
+     * @param array $row
+     *
+     * @return \Illuminate\Database\Eloquent\Model|null
+     */
     public function startRow(): int
     {
         return 2;
     }
 
     /**
-    * @param array $row
-    *
-    * @return \Illuminate\Database\Eloquent\Model|null
-    */
+     * @param array $row
+     *
+     * @return \Illuminate\Database\Eloquent\Model|null
+     */
     public function model(array $row)
     {
         $code = mt_rand(1, 999);
 
         $user = User::where('role', 'student')->where('number', $row[1])->first();
+        $teacher = User::where('role', 'teacher')->where('number', $row[3])->first();
 
         if ($user) {
             foreach ($user->levels as $level) {
                 if ($this->id == $level->id) {
                     $student = Student::create([
-                        'price' => $row[2],
-                        'currency' => $row[3],
-                        'month' => $row[4],
+                        'price' => $row[4],
+                        'currency' => $row[5],
+                        'month' => $row[6],
                         'code' => $code,
-                        'year' => $row[5],
+                        'year' => $row[7],
                         'user_id' => $user->id,
                         'level_id' => $this->id,
+                        'teacher_id' => $teacher->id,
                     ]);
 
-                    $inv = Invoice::count();
+                    // $inv = Invoice::count();
+
+                    // $yymm = Carbon::now()->format('ym');
+
+                    // if ($inv) {
+                    //     $number = $inv + 1;
+                    // } else if (Carbon::now() == date('Y-01-01')) {
+                    //     $number = 1;
+                    // } else {
+                    //     $number = 1;
+                    // }
 
                     $yymm = Carbon::now()->format('ym');
 
-                    if ($inv) {
-                        $number = $inv + 1;
-                    } else if (Carbon::now() == date('Y-01-01')) {
+                    if (date('Y-m-d') == date('Y-01-01')) {
                         $number = 1;
                     } else {
-                        $number = 1;
+                        $inv = Invoice::pluck('numberInv')->last();
+                        if ($inv) {
+                            $number = $inv + 1;
+                        } else {
+                            $number = 1;
+                        }
                     }
 
                     Invoice::create([
