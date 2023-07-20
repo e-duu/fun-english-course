cb248485f781cc39d3d2cf1650f5b671b1561f47
muji101 <mujikuwat91@gmail.com>
2023-07-20 07:06:24 +0700

    change ipaymu to production

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -68,11 +68,15 @@ class TransactionController extends Controller
             // $va           = '0000005747245474'; //get on iPaymu dashboard
             // $apiKey       = 'SANDBOX63A5AE9A-66D0-42A7-B9BD-69EEB2A2085D'; //get on iPaymu dashboard
 
-            $va           = '0000005725249265'; //get on iPaymu dashboard
-            $apiKey       = 'SANDBOXA4C06591-3721-4F8F-A55C-ED166AA057DB';
+            // $va           = '0000005725249265'; //get on iPaymu dashboard
+            // $apiKey       = 'SANDBOXA4C06591-3721-4F8F-A55C-ED166AA057DB';
 
-            $url          = 'https://sandbox.ipaymu.com/api/v2/payment'; // for development mode
-            // $url          = 'https://my.ipaymu.com/api/v2/payment'; // for production mode
+            //production
+            $va           = '1179008118606759'; //get on iPaymu dashboard
+            $apiKey       = 'C5F29392-0D73-4556-BCD6-38F22648A5DD';
+
+            // $url          = 'https://sandbox.ipaymu.com/api/v2/payment'; // for development mode
+            $url          = 'https://my.ipaymu.com/api/v2/payment'; // for production mode
 
             $method       = 'POST'; //method
 
